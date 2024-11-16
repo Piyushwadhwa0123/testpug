cc27726be95eb159524623940569d519cb7e0efd
Piyushwadhwa0123 <Piyushwadhwa0123@gmail.com>
2024-11-16 19:42:19 +0530

    ...final push again

--- app.js
+++ app.js
@@ -406,7 +406,7 @@ app.get('*', async (req, res) => {
                 if(allowedTemplates.includes(`${templateKey}${v1}${v2}`)){
                     res.render(`${templateKey}${v1}${v2}`, {
                         BrandName1: "O General",
-                        BrandName2: "O-GENERAL",
+                        BrandName2: "OGeneral",
                         BrandContent: brandContent.content,
                         NumValue: 9784467344
     
@@ -445,7 +445,7 @@ app.get('*', async (req, res) => {
                 if(allowedTemplates.includes(`${templateKey}${v1}${v2}`)){
                     res.render(`${templateKey}${v1}${v2}`, {            
                         BrandName1: "White Westinghouse",
-                        BrandName2: "White-Westinghouse",
+                        BrandName2: "WhiteWestinghouse",
                         BrandContent: brandContent.content,
                         NumValue: 9784467344
     
@@ -558,7 +558,7 @@ app.get('*', async (req, res) => {
                             
                                 NumValue: NumValue?.number,
                                 CityName1: "Navi Mumbai",   
-                                CityName2: "Navi-Mumbai"
+                                CityName2: "NaviMumbai"
                             })
 
                         }else{
@@ -722,11 +722,11 @@ app.get('*', async (req, res) => {
 
                                 res.render(`${templateKey}${v1}${v2}${v3}`, {
                                     BrandName1: "O General",
-                                    BrandName2: "O-GENERAL",
+                                    BrandName2: "OGeneral",
                                     BrandContent: brandContent.content,
                                     NumValue: NumValue?.number,
                                     CityName1: "Navi Mumbai",   
-                                    CityName2: "Navi-Mumbai"
+                                    CityName2: "NaviMumbai"
                                 })
                                 
                             }else{
@@ -742,7 +742,98 @@ app.get('*', async (req, res) => {
                         if(allowedTemplates.includes(`${templateKey}${v1}${v2}${v3}`)){
                             res.render(`${templateKey}${v1}${v2}${v3}`, {
                                 BrandName1: "O General",
-                                BrandName2: "O-GENERAL",
+                                BrandName2: "OGeneral",
+                                BrandContent: brandContent.content,
+                                NumValue: NumValue?.number,
+                                CityName1: afterSlash,
+                                CityName2: afterSlash
+                            }) 
+                        }else{
+
+                            return res.render("Repair-Service-Center", {
+                                NumValue: 9784467344
+                            })
+                        }
+                        
+
+                    }
+                    if(prefixRemoved === "BlueStar" ){
+
+
+                        if(afterSlash === "NaviMumbai"){
+
+
+                            if(allowedTemplates.includes(`${templateKey}${v1}${v2}${v3}`)){
+
+                                res.render(`${templateKey}${v1}${v2}${v3}`, {
+                                    BrandName1: "Blue Star",
+                                    BrandName2: "BlueStar",
+                                    BrandContent: brandContent.content,
+                                    NumValue: NumValue?.number,
+                                    CityName1: "Navi Mumbai",   
+                                    CityName2: "NaviMumbai"
+                                })
+                                
+                            }else{
+
+                                return res.render("Repair-Service-Center", {
+                                    NumValue: 9784467344
+                                })
+                            }
+                            
+    
+                        }
+
+                        if(allowedTemplates.includes(`${templateKey}${v1}${v2}${v3}`)){
+                            res.render(`${templateKey}${v1}${v2}${v3}`, {
+                                BrandName1: "Blue Star",
+                                BrandName2: "BlueStar",
+                                BrandContent: brandContent.content,
+                                NumValue: NumValue?.number,
+                                CityName1: afterSlash,
+                                CityName2: afterSlash
+                            }) 
+                        }else{
+
+                            return res.render("Repair-Service-Center", {
+                                NumValue: 9784467344
+                            })
+                        }
+                        
+
+                    }
+
+                    if(prefixRemoved === "WhiteWestinghouse" ){
+
+
+                        if(afterSlash === "NaviMumbai"){
+
+
+                            if(allowedTemplates.includes(`${templateKey}${v1}${v2}${v3}`)){
+
+                                res.render(`${templateKey}${v1}${v2}${v3}`, {
+                                    BrandName1: "White Westinghouse",
+                                    BrandName2: "WhiteWestinghouse",
+                                    BrandContent: brandContent.content,
+                                    NumValue: NumValue?.number,
+                                    CityName1: "Navi Mumbai",   
+                                    CityName2: "NaviMumbai"
+                                })
+                                
+                            }else{
+
+                                return res.render("Repair-Service-Center", {
+                                    NumValue: 9784467344
+                                })
+                            }
+                            
+    
+                        }
+
+                        if(allowedTemplates.includes(`${templateKey}${v1}${v2}${v3}`)){
+                            res.render(`${templateKey}${v1}${v2}${v3}`, {
+                                BrandName1: "White Westinghouse",
+                                BrandName2: "WhiteWestinghouse",
                                 BrandContent: brandContent.content,
                                 NumValue: NumValue?.number,
                                 CityName1: afterSlash,
@@ -767,7 +858,7 @@ app.get('*', async (req, res) => {
                                 BrandContent: brandContent.content,
                                 NumValue: NumValue?.number,
                                 CityName1: "Navi Mumbai",   
-                                CityName2: "Navi-Mumbai"
+                                CityName2: "NaviMumbai"
                             }) 
                         }else{
 
@@ -842,8 +933,8 @@ app.get('*', async (req, res) => {
                             NumValue: NumValue?.number,
                             CityName1: afterSlashCity,
                             CityName2: afterSlashCity,
-                            BrandName1: prefixRemoved === "OGeneral" ? "O General" : spacedPrefix,
-                            BrandName2: prefixRemoved === "OGeneral" ? "O-GENERAL" : hyphenatedPrefix,
+                            BrandName1: prefixRemoved === "OGeneral" ? "O General": spacedPrefix || "BlueStar"?"Blue Star" :spacedPrefix || "WhiteWestinghouse"?"White Westinghouse": spacedPrefix,
+                            BrandName2: prefixRemoved === "OGeneral" ? "OGeneral" : prefixRemoved || "BlueStar"?"BlueStar" :prefixRemoved || "WhiteWestinghouse"?"WhiteWestinghouse": prefixRemoved,
                             BrandContent: brandContent.content,
                             AreaName: `${afterSlashAreaName}`,
     
@@ -903,8 +994,8 @@ app.get('*', async (req, res) => {
                             NumValue: NumValue?.number,
                             CityName1: afterSlashCity,
                             CityName2: afterSlashCity,
-                            BrandName1: prefixRemoved === "OGeneral" ? "O General" : spacedPrefix,
-                            BrandName2: prefixRemoved === "OGeneral" ? "O-GENERAL" : hyphenatedPrefix,
+                            BrandName1: prefixRemoved === "OGeneral" ? "O General": spacedPrefix || "BlueStar"?"Blue Star" :spacedPrefix || "WhiteWestinghouse"?"White Westinghouse": spacedPrefix,
+                            BrandName2: prefixRemoved === "OGeneral" ? "OGeneral" : prefixRemoved || "BlueStar"?"BlueStar" :prefixRemoved || "WhiteWestinghouse"?"WhiteWestinghouse": prefixRemoved,
                             BrandContent: brandContent.content,
                             AreaName1: `${afterSlashAreaName.replace(/-/g, ' ')}`,
                             AreaName2: `${afterSlashAreaName}`,
